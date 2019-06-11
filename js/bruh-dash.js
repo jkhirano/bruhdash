8fc3631ed4c844a9e8b618dd2247f3a80c5b16cc
Janelle K Hirano <jaahnelle@gmail.com>
2019-06-10 22:56:22 -1000

    added up til slice

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -26,31 +26,38 @@ global.bruhdash = {
 
   // returns the index of the first matching element from right to left
   lastIndexOf: function (arr, x) {
-      return arr.lastIndexOf(x);
+    return arr.lastIndexOf(x);
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (arr) {
+    let lastElement = arr.pop();
+    return arr;
   },
-  
-  // returns an array with all falsey values removed
-  compact: function() {
 
+  // returns an array with all falsey values removed
+  compact: function (arr) {
+    let truthyBox = [];
+    for (let i = 0; i<arr.length; i++){
+      if (arr[i]){
+        truthyBox.push(arr[i]);
+      }
+    }
+    return truthyBox;
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (arr, x1, x2) {
+    return arr.slice(x1,x2);
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
+  drop: function () {
 
   },
 
   // returns a slice of array with n elements dropped from the end
-  dropRight: function() {
+  dropRight: function () {
 
   },
 
@@ -66,7 +73,7 @@ global.bruhdash = {
 
   // fills elements of array with specified value from the start index
   // up to but not including the end index
-  fill: function() {
+  fill: function () {
 
   },
 
@@ -81,18 +88,18 @@ global.bruhdash = {
   },
 
   // creates an array excluding all the specified values
-  without: function() {
+  without: function () {
 
   },
 
   // returns an array with specified values excluded
-  difference: function() {
+  difference: function () {
 
   },
 
   /*******************
    *  STRETCH GOALS! *
-   *******************/ 
+   *******************/
 
   // creates an array of grouped elements
   zip: function () {
@@ -105,36 +112,36 @@ global.bruhdash = {
   },
 
   // creates an array of elements into groups of length of specified size
-  chunk: function(){
+  chunk: function () {
 
   },
 
   // iterates over elements of a collection and invokes iteratee for each element
   // Note: this should work for arrays and objects
-  forEach: function() {
+  forEach: function () {
 
   },
 
   // creates an array of values by running each element in collection thru the iteratee
   // Note: this should work for arrays and objects
-  map: function() {
+  map: function () {
 
   },
 
   /*************************
    *  SUPER STRETCH GOALS!  *
-   *************************/ 
+   *************************/
 
   // iterates over elements of a collection and returns all elements that the predicate returns truthy for
   // Note: this should work for arrays and objects
-  filter: function() {
+  filter: function () {
 
   },
 
   // Reduces the collection to a value which is the accumulated result of running each element
   // in the collection through an iteratee
   // Note: this should work for arrays and objects
-  reduce: function() {
-    
+  reduce: function () {
+
   }
 };
