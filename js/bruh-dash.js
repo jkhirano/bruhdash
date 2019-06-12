b994a60e087ab658cbe781b65bdff245bdb781e8
Janelle K Hirano <jaahnelle@gmail.com>
2019-06-11 21:12:11 -1000

    added drop function

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -38,8 +38,8 @@ global.bruhdash = {
   // returns an array with all falsey values removed
   compact: function (arr) {
     let truthyBox = [];
-    for (let i = 0; i<arr.length; i++){
-      if (arr[i]){
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) {
         truthyBox.push(arr[i]);
       }
     }
@@ -48,100 +48,107 @@ global.bruhdash = {
 
   // creates a slice of an array from the start index up to but not including the end index
   slice: function (arr, x1, x2) {
-    return arr.slice(x1,x2);
+    return arr.slice(x1, x2);
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function () {
+  drop: function (arr, x) {
+    if (x === 0) {
+      return arr;
+    };
+      if(x) {
+        return arr.slice(x);
+      }else{
+        return arr.slice(1);
+      };
+    },
 
-  },
+      // returns a slice of array with n elements dropped from the end
+      dropRight: function () {
 
-  // returns a slice of array with n elements dropped from the end
-  dropRight: function () {
+      },
 
-  },
+    // creates a slice of an array with n elements taken from the beginning
+    take: function () {
 
-  // creates a slice of an array with n elements taken from the beginning
-  take: function () {
+    },
 
-  },
+    // creates a slice of an array with n elements taken from the end
+    takeRight: function () {
 
-  // creates a slice of an array with n elements taken from the end
-  takeRight: function () {
+    },
 
-  },
+    // fills elements of array with specified value from the start index
+    // up to but not including the end index
+    fill: function () {
 
-  // fills elements of array with specified value from the start index
-  // up to but not including the end index
-  fill: function () {
+    },
 
-  },
+    // removes all given values from an array
+    pull: function () {
 
-  // removes all given values from an array
-  pull: function () {
+    },
 
-  },
+    // removes elements of an array corresponding to the given indices
+    pullAt: function () {
 
-  // removes elements of an array corresponding to the given indices
-  pullAt: function () {
+    },
 
-  },
+    // creates an array excluding all the specified values
+    without: function () {
 
-  // creates an array excluding all the specified values
-  without: function () {
+    },
 
-  },
+    // returns an array with specified values excluded
+    difference: function () {
 
-  // returns an array with specified values excluded
-  difference: function () {
+    },
 
-  },
+    /*******************
+     *  STRETCH GOALS! *
+     *******************/
 
-  /*******************
-   *  STRETCH GOALS! *
-   *******************/
+    // creates an array of grouped elements
+    zip: function () {
 
-  // creates an array of grouped elements
-  zip: function () {
+    },
 
-  },
+    // creates an array of grouped elements in their pre-zip configuration
+    unzip: function () {
 
-  // creates an array of grouped elements in their pre-zip configuration
-  unzip: function () {
+    },
 
-  },
+    // creates an array of elements into groups of length of specified size
+    chunk: function () {
 
-  // creates an array of elements into groups of length of specified size
-  chunk: function () {
+    },
 
-  },
+    // iterates over elements of a collection and invokes iteratee for each element
+    // Note: this should work for arrays and objects
+    forEach: function () {
 
-  // iterates over elements of a collection and invokes iteratee for each element
-  // Note: this should work for arrays and objects
-  forEach: function () {
+    },
 
-  },
+    // creates an array of values by running each element in collection thru the iteratee
+    // Note: this should work for arrays and objects
+    map: function () {
 
-  // creates an array of values by running each element in collection thru the iteratee
-  // Note: this should work for arrays and objects
-  map: function () {
+    },
 
-  },
+    /*************************
+     *  SUPER STRETCH GOALS!  *
+     *************************/
 
-  /*************************
-   *  SUPER STRETCH GOALS!  *
-   *************************/
+    // iterates over elements of a collection and returns all elements that the predicate returns truthy for
+    // Note: this should work for arrays and objects
+    filter: function () {
 
-  // iterates over elements of a collection and returns all elements that the predicate returns truthy for
-  // Note: this should work for arrays and objects
-  filter: function () {
+    },
 
-  },
+    // Reduces the collection to a value which is the accumulated result of running each element
+    // in the collection through an iteratee
+    // Note: this should work for arrays and objects
+    reduce: function () {
 
-  // Reduces the collection to a value which is the accumulated result of running each element
-  // in the collection through an iteratee
-  // Note: this should work for arrays and objects
-  reduce: function () {
-
-  }
-};
+    }
+  };
